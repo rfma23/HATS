2ecb07501e46c7f53e241955389acc4dbe0fb2b0
JoshPoravanthattil <joshbp9@gmail.com>
2022-10-04 18:32:45 -0400

    Added data setup code

--- c_HATS.cpp
+++ c_HATS.cpp
@@ -1,5 +1,13 @@
-#include "iostream"
-#include "vector"
+#include <iostream>
+#include <fstream>
+#include <stdint.h>
+
+#include <vector>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
 
 struct LRBound {
     int left = 0;
@@ -7,10 +15,10 @@ struct LRBound {
 };
 
 struct Event {
-    int x = 0;
-    int y = 0;
-    int ts = 0;
-    int polarity = 0;
+    unsigned int x = 0;
+    unsigned int y = 0;
+    unsigned int ts = 0;
+    bool polarity;
 };
 
 struct TrainingSample {
@@ -20,11 +28,22 @@ struct TrainingSample {
 
 int main(void) 
 {
-    struct LRBound chicken;
-    struct TrainingSample leg;
-    leg.features.push_back(15);
-    std::cout << "chicken val " << chicken.left;
-    std::cout << "oof: " << leg.features.at(0);
+    // struct LRBound potato;
+    // struct TrainingSample leg;
+    // leg.features.push_back(15);
+    // std::cout << "chicken val " << potato.left;
+    // std::cout << "oof: " << potato.features.at(0);
+
+    char data[1];
+    ifstream infile("./dat/training/0/00002.bin", ios::in | ios::binary);
+    if(!infile) {
+        cout << "AYOOOO";
+    }
+    cout << "Ok we are trying to read";
+    infile.read((char *) &data, sizeof(data));
+    cout << "data" << data;
+    // std::cout << "Reading data from file";
+    // infile >> data;
+    // std::cout << data;
     return 0;
 }
-
